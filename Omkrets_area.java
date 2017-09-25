9e81e529f7fd58e98aa021b394b032eb46aa5112
Benjamin Bylund <bybe16@nti.umea>
2017-09-25 09:48:28 +0200

    Uppgift 3.3

---
+++ Omkrets_area.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+/*
+	Beräknar omkrets och area av en cirkel
+	Benjamin Bylund
+	2017-09-24
+*/
+
+public class Omkrets_area{
+	public static void main(String[] args) {
+		
+		String radie = JOptionPane.showInputDialog("Test");
+		
+		double radie1 = Double.parseDouble(radie);
+		double pi = 3.1415926536;
+		double area = pi * (radie1 * radie1);
+		double omkrets = (2 * pi) * radie1;
+
+
+		JOptionPane.showMessageDialog(null, area);
+	}
+}
